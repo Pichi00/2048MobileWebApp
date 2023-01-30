2bab29d8c19342ef0ebbd12048fcd299369bf33c
Piotr Kołodziejski <piotr.kolodziejski00@hotmail.com>
2023-01-30 13:08:29 +0100

    Add setting up  game with script

--- script/script.js
+++ script/script.js
@@ -1,1 +1,31 @@
-const board = document.getElementById("game-board");
+const gameBoard = document.getElementById("game-board");
+const GRID_SIZE = 4;
+
+let boardArray;
+let score = 0;
+
+window.onload = function () {
+  setUpGame();
+};
+
+function setUpGame() {
+  console.log("Setting up game");
+
+  boardArray = [
+    [0, 0, 0, 0],
+    [0, 0, 0, 0],
+    [0, 0, 0, 0],
+    [0, 0, 0, 0],
+  ];
+
+  for (let i = 0; i < GRID_SIZE; i++) {
+    for (let j = 0; j < GRID_SIZE; j++) {
+      let cell = document.createElement("div");
+      cell.className = "cell";
+      let num = boardArray[i][j];
+      gameBoard.append(cell);
+    }
+  }
+
+  console.log("Game set up");
+}
